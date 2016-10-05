5c436f752b1ff28f5fbf98a3ddf2999038e228a7
herasymj <jennifer.herasymuik@gmail.com>
2016-10-05 16:36:05 -0600

    Started the class for list element. going to finish rest at home.

--- Lab3/src/List.java
+++ Lab3/src/List.java
@@ -1,3 +1,6 @@
+//package lab3;
+import java.util.Scanner;
+
 /*
  * Lab 3; This file runs the link list
  * 
@@ -9,8 +12,11 @@
 public class List {
 
 	public static void main(String[] args) {
-		System.out.println("This program allows you to amnipulate a linked list.");
+		System.out.println("This program allows you to amnipulate a linked list of INTEGERS.");
 		char selection = 'a';
+		
+		//scanner so we can input values for the selection char
+		Scanner in = new Scanner(System.in);
 		
 		while((selection != 'Q') && (selection != 'q')){
 		System.out.println("Select one of the options below:");
@@ -20,8 +26,12 @@ public class List {
 		System.out.println("d) Print from end of list");
 		System.out.println("e) Print from start of list");
 		System.out.println("q) Quit program");
-		}
 		
+		
+		
+		
+		}
+		in.close();
 		System.out.println("Quitting...");
 	}
 
--- Lab3/src/ListElement.java
+++ Lab3/src/ListElement.java
@@ -1,4 +1,22 @@
 
 public class ListElement {
+	
+	private ListElement next;
+	private ListElement previous;
+	private int data;
+	
+	public ListElement(){
+		this.data = 0;
+		this.next = null;
+		this.previous = null;
+	}
+	
+	public ListElement(int d, ListElement n, ListElement p){
+		this.data = d;
+		this.next = n;
+		this.previous = p;
+	}
 
+	
+	
 }
