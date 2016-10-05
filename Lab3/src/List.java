d2583072ad5f11d5af7f3610fba8c4b491a14756
herasymj <jennifer.herasymuik@gmail.com>
2016-10-05 16:27:33 -0600

    Added messages so user can navigate program

--- Lab3/src/List.java
+++ Lab3/src/List.java
@@ -10,13 +10,19 @@ public class List {
 
 	public static void main(String[] args) {
 		System.out.println("This program allows you to amnipulate a linked list.");
+		char selection = 'a';
+		
+		while((selection != 'Q') && (selection != 'q')){
 		System.out.println("Select one of the options below:");
 		System.out.println("a) Add Node");
 		System.out.println("b) Find Node");
 		System.out.println("c) Delete a Node");
 		System.out.println("d) Print from end of list");
 		System.out.println("e) Print from start of list");
-
+		System.out.println("q) Quit program");
+		}
+		
+		System.out.println("Quitting...");
 	}
 
 }
