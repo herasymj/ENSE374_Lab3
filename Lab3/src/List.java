3b437f4ac0483f89332a9d8256f93fef0bf14984
herasymj <jennifer.herasymuik@gmail.com>
2016-10-26 00:29:51 -0600

    Update final version of file

--- Lab3/src/List.java
+++ Lab3/src/List.java
@@ -1,4 +1,4 @@
-//package lab3;
+package lab3;
 import java.util.Scanner;
 
 /*
@@ -13,22 +13,56 @@ public class List {
 
 	public static void main(String[] args) {
 		System.out.println("This program allows you to amnipulate a linked list of INTEGERS.");
-		char selection = 'a';
+		int selection = 1;
+		
+		//list element that represents head of linked list
+		ListElement head = new ListElement();
 		
 		//scanner so we can input values for the selection char
 		Scanner in = new Scanner(System.in);
 		
-		while((selection != 'Q') && (selection != 'q')){
+		while(selection != 0){
 		System.out.println("Select one of the options below:");
-		System.out.println("a) Add Node");
-		System.out.println("b) Find Node");
-		System.out.println("c) Delete a Node");
-		System.out.println("d) Print from end of list");
-		System.out.println("e) Print from start of list");
-		System.out.println("q) Quit program");
+		System.out.println("1: Add Node");
+		System.out.println("2: Find Node");
+		System.out.println("3: Delete a Node");
+		//System.out.println("4: Print from end of list");
+		System.out.println("5: Print from start of list");
+		System.out.println("0: Quit program");
+		System.out.println("Selection: ");
 		
+		selection = in.nextInt();
 		
-		
+		if(selection == 1){
+			System.out.println("Enter value of integer to add to list: ");
+			int num = in.nextInt();
+			ListElement newElement = new ListElement();
+			newElement.setData(num);
+			head.addElement(newElement);
+			
+		}
+		else if(selection == 2){
+			System.out.println("Search to see if value is in list: ");
+			int num = in.nextInt();
+			if (head.getElement(num) != null){
+				System.out.println("Element was in list!");
+			}
+		}
+		else if(selection == 3){
+			System.out.println("Enter number to be deleted: ");
+			int num = in.nextInt();
+			head = head.deleteElement(num);
+			if(head != null){
+				System.out.println("Item deleted...");
+			}
+		}
+		//else if(selection == 4){}
+		else if(selection == 5){
+			head.print();
+		}
+		else{
+			System.out.println("Please enter one of the letter values show in list...");
+		}
 		
 		}
 		in.close();
